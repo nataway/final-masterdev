5c076a9be035fcf1a3b8b90cd31b10f1147c2cb7
kingu <43698423+nataway@users.noreply.github.com>
2022-08-21 10:21:57 +0700

    Update Airflow-SparkStreaming-server23.py

--- airflow/dags/Airflow-SparkStreaming-server23.py
+++ airflow/dags/Airflow-SparkStreaming-server23.py
@@ -8,7 +8,7 @@ dag = DAG('SparkToPostgres', description='Hello World DAG',
           start_date=datetime(2022, 8, 13), catchup=False)
 
 spark_submit_local = SparkSubmitOperator(
-		application ='/home/chibm/spark_gtvt/streaming.py' ,
+		application ='SparkStreaming-postgres.py' ,
 		conn_id= 'spark_local', 
 		task_id='spark_submit_task', 
         packages = "org.apache.spark:spark-sql-kafka-0-10_2.12:3.0.1,org.postgresql:postgresql:42.2.18",

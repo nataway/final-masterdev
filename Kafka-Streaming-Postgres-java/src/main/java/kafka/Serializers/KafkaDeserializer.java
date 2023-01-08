6306b5fa02b26c4da1ebe9ab16b43ddfe36cbd05
nataway <buiminhchi0405@gmail.com>
2023-01-08 09:03:50 +0700

    add

---
+++ Kafka-Streaming-Postgres-java/src/main/java/kafka/Serializers/KafkaDeserializer.java
@@ -0,0 +1,7 @@
+package kafka.Serializers;
+
+import com.google.protobuf.Parser;
+import org.apache.kafka.common.serialization.Deserializer;
+
+public class KafkaDeserializer {
+}
---
+++ Kafka-Streaming-Postgres-java/src/main/java/kafka/Serializers/KafkaSerializer.java
@@ -0,0 +1,9 @@
+package kafka.Serializers;
+
+
+import com.google.protobuf.Parser;
+import org.apache.kafka.common.serialization.Serializer;
+
+
+public class KafkaSerializer {
+}
